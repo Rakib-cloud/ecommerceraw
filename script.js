9df0fa791d52a0175982f367bc279c367853b82e
Rakib-cloud <ri1641715@gmail.com>
2023-01-11 16:13:45 +0600

    update2.0

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 //sign in and sign up page work
-
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-});
-
-
-
-
+const bar=document.getElementById('bar');
+const close=document.getElementById('close');
+const nav=document.getElementById('navbar');
+if(bar){
+    bar.addEventListener('click',()=>{
+nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
 
 
 
